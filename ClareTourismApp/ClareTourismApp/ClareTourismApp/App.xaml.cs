e4a7fc6939ea3043adff8f151cf0eb088082751a
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2018-12-17 18:25:18 +0000

    Added Mainpage Navigation Buttons (Basic App Layout)

--- ClareTourismApp/ClareTourismApp/ClareTourismApp/App.xaml.cs
+++ ClareTourismApp/ClareTourismApp/ClareTourismApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace ClareTourismApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- ClareTourismApp/ClareTourismApp/ClareTourismApp/MainPage.xaml.cs
+++ ClareTourismApp/ClareTourismApp/ClareTourismApp/MainPage.xaml.cs
@@ -12,6 +12,53 @@ namespace ClareTourismApp
         public MainPage()
         {
             InitializeComponent();
+            SetupImagesOnMainPage();
+        }
+
+        private void Cliffsbtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CliffsOfMoher());
+        }
+
+        private void Burrenbtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Burren());
+        }
+
+        private void Bunrattybtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Bunratty());
+        }
+
+        private void AilweeCavebtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AilweeCave());
+        }
+
+        private void Doolinbtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Doolin());
+        }
+
+        private void Craggaunowenbtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Craggaunowen());
+        }
+
+        private void Lahinchbtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Lahinch());
+        }
+
+        private void SetupImagesOnMainPage()
+        {
+            //  get the assembly
+            var assembly = typeof(MainPage);
+
+            string strFilename = "ClareTourismApp.Assets.Images.CliffsSmall.jpg";
+
+            cliffsSmall.Source = ImageSource.FromResource(strFilename, assembly);
+
         }
     }
 }
