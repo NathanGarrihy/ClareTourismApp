20b049aff2bf918409085451128cee5e8da0a991
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2018-12-20 04:48:21 +0000

    Created DoolinCave and Craggaunowen Pages

--- ClareTourismApp/ClareTourismApp/ClareTourismApp/Craggaunowen.xaml.cs
+++ ClareTourismApp/ClareTourismApp/ClareTourismApp/Craggaunowen.xaml.cs
@@ -15,6 +15,27 @@ namespace ClareTourismApp
 		public Craggaunowen ()
 		{
 			InitializeComponent ();
+            SetUpImagesOnPage();
 		}
-	}
+
+        private void SetUpImagesOnPage()
+        {
+            var assembly = typeof(Craggaunowen);
+
+            string craggaunowenPic1 = "ClareTourismApp.Assets.Images.craggaunowen1.jpg";
+            craggaunowen1.Source = ImageSource.FromResource(craggaunowenPic1, assembly);
+
+            string craggaunowenPic2 = "ClareTourismApp.Assets.Images.craggaunowen2.jpg";
+            craggaunowen2.Source = ImageSource.FromResource(craggaunowenPic2, assembly);
+
+            string craggaunowenPic3 = "ClareTourismApp.Assets.Images.craggaunowen3.jpg";
+            craggaunowen3.Source = ImageSource.FromResource(craggaunowenPic3, assembly);
+
+            string largeCraggaunowen = "ClareTourismApp.Assets.Images.craggaunowenLarge.jpg";
+            craggaunowenLarge.Source = ImageSource.FromResource(largeCraggaunowen, assembly);
+
+            string largeCraggaunowen2 = "ClareTourismApp.Assets.Images.craggaunowenLarge2.jpg";
+            craggaunowenLarge2.Source = ImageSource.FromResource(largeCraggaunowen2, assembly);
+        }
+    }
 }
--- ClareTourismApp/ClareTourismApp/ClareTourismApp/DoolinCave.xaml.cs
+++ ClareTourismApp/ClareTourismApp/ClareTourismApp/DoolinCave.xaml.cs
@@ -15,6 +15,25 @@ namespace ClareTourismApp
 		public DoolinCave ()
 		{
 			InitializeComponent ();
-		}
-	}
+            SetUpImagesOnPage();
+        }
+
+        private void SetUpImagesOnPage()
+        {
+            var assembly = typeof(DoolinCave);
+
+            string smallDoolinCave1 = "ClareTourismApp.Assets.Images.doolinCaveSmall1.jpg";
+            doolinCaveSmall1.Source = ImageSource.FromResource(smallDoolinCave1, assembly);
+
+            string smallDoolinCave2 = "ClareTourismApp.Assets.Images.doolinCaveSmall2.jpg";
+            doolinCaveSmall2.Source = ImageSource.FromResource(smallDoolinCave2, assembly);
+
+            string smallDoolinCave3 = "ClareTourismApp.Assets.Images.doolinCaveSmall3.jpg";
+            doolinCaveSmall3.Source = ImageSource.FromResource(smallDoolinCave3, assembly);
+
+            string largeBirds1 = "ClareTourismApp.Assets.Images.doolinCaveLarge.jpg";
+            doolinCaveLarge.Source = ImageSource.FromResource(largeBirds1, assembly);
+            
+        }
+    }
 }
